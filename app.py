29f74f3f3d4e39b3a5b607e2d6c56818084124f6
saisrinivas194 <pedhapollasaisrinivas@gmail.com>
2025-06-22 17:04:07 -0400

    updated

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import traceback
 from flask_cors import CORS
 import jedi
 import re
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -68,4 +69,5 @@ def suggest():
     return jsonify({'suggestions': suggestions + comment_suggestions})
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True) 
